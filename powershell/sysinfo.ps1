2a262f15796211def8ffde1a019b0272297ccdd2
thom4kw <thoma4kw@mail.uc.edu>
2023-09-20 20:51:41 -0400

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,11 @@
+function getIP{ (get-netipaddress).ipv4address | Select-String "192*"}
+
+Write-host(getIP)
+
+$IP = getIP 
+
+Write-Host("This machine's IP is $IP")
+
+Write-Host("This machine's IP is {0}" -f $IP) 
+
+$IP
