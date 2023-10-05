b42deeb39aa83b76b2f5fc52829b4097de153a90
thoma4kw <112356628+thoma4kw@users.noreply.github.com>
2023-10-05 15:56:20 -0400

    Update Project1.py
    from github

--- python/Project1.py
+++ python/Project1.py
@@ -2,4 +2,19 @@ pip install psutil
 
 import psutil
 
-def get
+def get_free_space(path='/'):
+    try:
+        disk_usage = psutil.disk_usage(path)
+        return disk_usage.free
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == "__main__":
+    path = '/'  # You can specify a different path if needed, e.g., '/home' for your home directory
+    free_space_bytes = get_free_space(path)
+    free_space_gb = round(free_space_bytes / (1024 ** 3), 2)  # Convert bytes to gigabytes
+
+    if isinstance(free_space_bytes, int):
+        print(f"Free hard drive space on '{path}': {free_space_gb} GB")
+    else:
+        print(f"Failed to retrieve free space: {free_space_bytes}")
