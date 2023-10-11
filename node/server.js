7dbb0437201eae7fbeb52e68c89e69ac262ea0c5
thoma4kw <112356628+thoma4kw@users.noreply.github.com>
2023-10-10 23:22:40 -0400

    Update server.js

--- node/server.js
+++ node/server.js
@@ -1,41 +1,50 @@
-
 var http = require("http");
 var fs = require("fs");
 var os = require("os");
 var ip = require('ip');
 
-http.createServer(function(req, res){
-
-    if (req.url === "/") {
-        fs.readFile("./public/index.html", "UTF-8", function(err, body){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(body);
+http.createServer(function(req, res) {
+  if (req.url === "/") {
+    fs.readFile("./public/index.html", "UTF-8", function(err, body) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(body);
     });
-}
-    else if(req.url.match("/sysinfo")) {
-        myHostName=os.hostname();
-        html=`    
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Node JS Response</title>
-          </head>
-          <body>
-            <p>Hostname: ${myHostName}</p>
-            <p>IP: ${ip.address()}</p>
-            <p>Server Uptime:<?php include 'uptime.php'; echo $uptime; ?></p>
-            <p>Total Memory:<?php $memory_limit = ini_get('memory_limit'); echo $memory_limit;?> </p>
-            <p>Free Memory: </p>
-            <p>Number of CPUs: $cpu_count = intval(trim(shell_exec('nproc'))); echo "$cpu_count" </p>            
-          </body>
-        </html>` 
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(html);
-    }
-    else {
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end(`404 File Not Found at ${req.url}`);
-    }
+  } else if (req.url.match("/sysinfo")) {
+    
+    var uptime = os.uptime();
+    var days = Math.floor(uptime / 86400);
+    var hours = Math.floor((uptime % 86400) / 3600);
+    var minutes = Math.floor((uptime % 3600) / 60);
+    var seconds = Math.floor(uptime % 60);
+
+    var totalMemory = os.totalmem();
+      
+    var freeMemory = os.freemem();
+
+    var numberOfCPUs = os.cpus().length;
+
+    var html = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Node JS Response</title>
+</head>
+<body>
+<p>Hostname: ${os.hostname()}</p>
+<p>IP: ${ip.address()}</p>
+<p>Server Uptime: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds</p>
+<p>Total Memory: ${totalMemory / 1024 / 1024} MB</p>
+<p>Free Memory: ${freeMemory / 1024 / 1024} MB</p>
+<p>Number of CPUs: ${numberOfCPUs}</p>
+</body>
+</html>`;
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(html);
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end(`404 File Not Found at ${req.url}`);
+  }
 }).listen(3000);
 
 console.log("Server listening on port 3000");
