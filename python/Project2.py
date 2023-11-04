7a67d3738917394bf5f585b81954208ef7285332
thoma4kw <112356628+thoma4kw@users.noreply.github.com>
2023-11-04 15:05:59 -0400

    Update Project2.py

--- python/Project2.py
+++ python/Project2.py
@@ -1,1 +1,41 @@
-test
+pip install dash plotly pandas
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+# Sample data
+data = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D'],
+    'Value': [10, 15, 7, 12]
+})
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Simple Dash Example"),
+    dcc.Graph(id='bar-chart'),
+    dcc.Dropdown(
+        id='category-dropdown',
+        options=[
+            {'label': category, 'value': category}
+            for category in data['Category']
+        ],
+        value=data['Category'][0]
+    )
+])
+
+@app.callback(
+    Output('bar-chart', 'figure'),
+    Input('category-dropdown', 'value')
+)
+def update_bar_chart(selected_category):
+    filtered_data = data[data['Category'] == selected_category]
+    fig = px.bar(filtered_data, x='Category', y='Value', title=f'Bar Chart for Category {selected_category}')
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
