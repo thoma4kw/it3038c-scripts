58f98a8d53d655dcc30bf85adacf68988de1c79e
thoma4kw <112356628+thoma4kw@users.noreply.github.com>
2023-11-04 14:52:45 -0400

    Update lab8.py

--- python/lab8.py
+++ python/lab8.py
@@ -1,1 +1,13 @@
-test
+pip install googlesearch-python
+
+from googlesearch import search
+
+# Define the search query
+query = "NFL scores"
+
+# Perform the Google search and extract search results
+search_results = search(query, num=10, stop=10, pause=2.0)
+
+# Search results and print them
+for result in search_results:
+    print(result)
